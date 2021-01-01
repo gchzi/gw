083913f1facc59581a4b6c7e147c746a7d0f557b
gchzi <920276367@qq.com>
2021-01-01 20:15:36 +0800

    zheshidectj

---
+++ js/index.js
@@ -0,0 +1,85 @@
+
+var arrList = [
+    {
+        title: '低压焊机',
+        image: './images/420.jpg',
+        dsec: '低压焊机1111',
+        typeCont: '85',
+        name: '君邦',
+        specs: '超取满1.500包邮',
+        type: '满额赠礼；满件折扣'
+    },
+    {
+        title: '低压焊机11',
+        image: './images/400.jpg',
+        dsec: '低压焊机1111',
+        typeCont: '8511',
+        name: '君邦11',
+        specs: '超取满1.500包邮111',
+        type: '满额赠礼；满件折扣111'
+    },
+    {
+        title: '低压焊机22',
+        image: './images/315.jpg',
+        dsec: '低压焊机222',
+        typeCont: '8522',
+        name: '君邦222',
+        specs: '超取满1.500包邮1221',
+        type: '满额赠礼；满件折扣1222'
+    },
+    {
+        title: '低压焊机2233',
+        image: './images/500.jpg',
+        dsec: '低压焊机22233',
+        typeCont: '852233',
+        name: '君邦222',
+        specs: '超取满1.500包邮12213',
+        type: '满额赠礼；满件折扣122233'
+    },
+    {
+        title: '低压焊机2233',
+        image: './images/500.jpg',
+        dsec: '低压焊机22233',
+        typeCont: '852233',
+        name: '君邦222',
+        specs: '超取满1.500包邮12213',
+        type: '满额赠礼；满件折扣122233'
+    },
+    {
+        title: '低压焊机2233',
+        image: './images/500.jpg',
+        dsec: '低压焊机22233',
+        typeCont: '852233',
+        name: '君邦222',
+        specs: '超取满1.500包邮12213',
+        type: '满额赠礼；满件折扣122233'
+    }
+]
+function addList() {
+
+    for (var i = 0; i < arrList.length; i++) {
+        // console.log(arrList[i], 5646)
+        // 传入模板字符串
+        let html = `
+        <div class="list_itme">
+            <div class="imgs_left ">
+                <img class="imgs" src="${arrList[i].image}" />
+            </div>
+            <div class="content_text">
+                <h6 class="title">${arrList[i].title}</h6>
+                <ul class="item_text">
+                    <li><a href="javascript:;">${arrList[i].dsec}</a></li>
+                    <li>型号:${arrList[i].typeCont}</li>
+                    <li>品牌 ${arrList[i].name}</li>
+                    <li>规格 ${arrList[i].specs}</li>
+                    <li>类型 ${arrList[i].type}</li>
+                </ul>
+            </div>
+        </div>
+        `
+        console.log(html, 'hmtl')
+        // content_list获取这个元素
+        $('#content_list').append(html)
+    }
+}
+addList()
